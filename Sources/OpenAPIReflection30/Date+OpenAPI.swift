0b0a992cbc46b757f08b632a1b0f043fcf17de4f
Mathew Polzin <matt.polzin@gmail.com>
2025-06-11 16:11:07 -0500

    just try and subvert syntax error

--- Sources/OpenAPIReflection30/Date+OpenAPI.swift
+++ Sources/OpenAPIReflection30/Date+OpenAPI.swift
@@ -9,42 +9,49 @@ import Foundation
 import OpenAPIKit30
 
 extension Date: DateOpenAPISchemaType {
-	public static func dateOpenAPISchemaGuess(using encoder: JSONEncoder) -> JSONSchema? {
+  public static func dateOpenAPISchemaGuess(using encoder: JSONEncoder) -> JSONSchema? {
 
-		switch encoder.dateEncodingStrategy {
-		case .deferredToDate, .custom:
-			// I don't know if we can say anything about this case without
-			// encoding the Date and looking at it, which is what `primitiveGuess()`
-			// does.
-			return nil
+    switch encoder.dateEncodingStrategy {
+    case .deferredToDate, .custom:
+      // I don't know if we can say anything about this case without
+      // encoding the Date and looking at it, which is what `primitiveGuess()`
+      // does.
+      return nil
 
-		case .secondsSince1970,
-			 .millisecondsSince1970:
-			return .number(format: .double)
+    case .secondsSince1970,
+      .millisecondsSince1970:
+      return .number(format: .double)
 
-		case .iso8601:
-			return .string(format: .dateTime)
+    case .iso8601:
+      return .string(format: .dateTime)
 
-	#if !TARGET_OS_LINUX || swift(<5.10)
-		case .formatted(let formatter):
-			let hasTime = formatter.timeStyle != .none
-			let format: JSONTypeFormat.StringFormat = hasTime ? .dateTime : .date
+  #if !TARGET_OS_LINUX || swift(<5.10)
+    case let .formatted(formatter):
+      return fromFormatter(formatter)
+  #endif
 
-			return .string(format: format)
-	#endif
-
-@unknown default:
-            return nil
-        }
-	}
+    @unknown default:
+      return nil
+    }
+  }
 }
 
-extension Date: OpenAPIEncodedSchemaType {
-    public static func openAPISchema(using encoder: JSONEncoder) throws -> JSONSchema {
-        guard let dateSchema: JSONSchema = try openAPISchemaGuess(for: Date(), using: encoder) else {
-            throw OpenAPI.TypeError.unknownSchemaType(type(of: self))
-        }
 
-        return dateSchema
+#if !TARGET_OS_LINUX || swift(<5.10)
+fileprivate func fromFormatter(_ formatter: DateFormatter) -> JSONSchema {
+  let hasTime = formatter.timeStyle != .none
+  let format: JSONTypeFormat.StringFormat = hasTime ? .dateTime : .date
+
+  return .string(format: format)
+}
+#endif
+
+extension Date: OpenAPIEncodedSchemaType {
+  public static func openAPISchema(using encoder: JSONEncoder) throws -> JSONSchema {
+    guard let dateSchema: JSONSchema = try openAPISchemaGuess(for: Date(), using: encoder) else {
+      throw OpenAPI.TypeError.unknownSchemaType(type(of: self))
     }
+
+    return dateSchema
+  }
 }
