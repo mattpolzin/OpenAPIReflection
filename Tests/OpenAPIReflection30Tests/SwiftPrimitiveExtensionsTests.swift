90b20df61bd5c1655c8bd1932cfc0de7c260e8d4
Mathew Polzin <matt.polzin@gmail.com>
2026-02-20 10:00:36 -0600

    add more test cases for primitive conformances

--- Tests/OpenAPIReflection30Tests/SwiftPrimitiveExtensionsTests.swift
+++ Tests/OpenAPIReflection30Tests/SwiftPrimitiveExtensionsTests.swift
@@ -21,31 +21,39 @@ class SwiftPrimitiveTypesTests: XCTestCase {
         XCTAssertEqual(Date?.dateOpenAPISchemaGuess(using: testEncoder), .string(format: .dateTime, required: false))
     }
 
-    func test_RawNodeType() {
-        XCTAssertEqual(try! RawRepStringEnum.rawOpenAPISchema(), .string)
-        XCTAssertEqual(try! RawRepIntEnum.rawOpenAPISchema(), .integer)
+    func test_RawNodeType() throws {
+        XCTAssertEqual(try RawRepStringEnum.rawOpenAPISchema(), .string)
+        XCTAssertEqual(try RawRepIntEnum.rawOpenAPISchema(), .integer)
     }
 
-    func test_OptionalRawRepresentable() {
-        XCTAssertEqual(try! RawRepStringEnum?.rawOpenAPISchema(), .string(required: false))
+    func test_OptionalRawRepresentable() throws {
+        XCTAssertEqual(try RawRepStringEnum?.rawOpenAPISchema(), .string(required: false))
 
-        XCTAssertEqual(try! RawRepIntEnum?.rawOpenAPISchema(), .integer(required: false))
+        XCTAssertEqual(try RawRepIntEnum?.rawOpenAPISchema(), .integer(required: false))
     }
 
-    func test_OptionalRawNodeType() {
-        XCTAssertEqual(try! RawRepStringEnum?.rawOpenAPISchema(), .string(required: false))
+    func test_OptionalRawNodeType() throws {
+        XCTAssertEqual(try RawRepStringEnum?.rawOpenAPISchema(), .string(required: false))
 
-        XCTAssertEqual(try! RawRepIntEnum?.rawOpenAPISchema(), .integer(required: false))
+        XCTAssertEqual(try RawRepIntEnum?.rawOpenAPISchema(), .integer(required: false))
     }
 
-    func test_DoubleWrappedRawNodeType() {
-        XCTAssertEqual(try! RawRepStringEnum??.rawOpenAPISchema(), .string(required: false))
+    func test_DoubleWrappedRawNodeType() throws {
+        XCTAssertEqual(try RawRepStringEnum??.rawOpenAPISchema(), .string(required: false))
 
-        XCTAssertEqual(try! RawRepIntEnum??.rawOpenAPISchema(), .integer(required: false))
+        XCTAssertEqual(try RawRepIntEnum??.rawOpenAPISchema(), .integer(required: false))
 
-        XCTAssertEqual(try! RawRepStringEnum??.rawOpenAPISchema(), .string(required: false))
+        XCTAssertEqual(try RawRepStringEnum??.rawOpenAPISchema(), .string(required: false))
 
-        XCTAssertEqual(try! RawRepIntEnum??.rawOpenAPISchema(), .integer(required: false))
+        XCTAssertEqual(try RawRepIntEnum??.rawOpenAPISchema(), .integer(required: false))
+    }
+
+    func test_arraySchemaType() throws {
+        XCTAssertEqual(try [EncodedSchemaStruct].openAPISchema(using: testEncoder), .array(items: .string))
+    }
+
+    func test_dictSchemaType() throws {
+        XCTAssertEqual(try [String: EncodedSchemaStruct].openAPISchema(using: testEncoder), .object(additionalProperties: .b(.string)))
     }
 
     static let localTestEncoder = JSONEncoder()
@@ -59,4 +67,10 @@ fileprivate enum RawRepStringEnum: String, RawOpenAPISchemaType, CaseIterable, C
 fileprivate enum RawRepIntEnum: Int, RawOpenAPISchemaType {
     case one
     case two
+}
+
+fileprivate struct EncodedSchemaStruct: OpenAPIEncodedSchemaType {
+    static func openAPISchema(using encoder: JSONEncoder) throws -> JSONSchema {
+        .string
+    }
 }
