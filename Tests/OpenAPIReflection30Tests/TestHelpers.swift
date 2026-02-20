2acac4d541a50502d5036076ea1cdfc5cc3890a6
Mathew Polzin <matt.polzin@gmail.com>
2026-02-20 09:47:30 -0600

    remove unneeded conditions in test helpers

--- Tests/OpenAPIReflection30Tests/TestHelpers.swift
+++ Tests/OpenAPIReflection30Tests/TestHelpers.swift
@@ -10,16 +10,9 @@ import XCTest
 
 let testEncoder = { () -> JSONEncoder in
     let encoder = JSONEncoder()
-    if #available(macOS 10.13, *) {
-        encoder.dateEncodingStrategy = .iso8601
-        encoder.keyEncodingStrategy = .useDefaultKeys
-        encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
-    }
-    #if os(Linux)
     encoder.dateEncodingStrategy = .iso8601
     encoder.keyEncodingStrategy = .useDefaultKeys
     encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
-    #endif
     return encoder
 }()
 
@@ -29,29 +22,19 @@ func testStringFromEncoding<T: Encodable>(of entity: T) throws -> String? {
 
 let testDecoder = { () -> JSONDecoder in
     let decoder = JSONDecoder()
-    if #available(macOS 10.12, *) {
-        decoder.dateDecodingStrategy = .iso8601
-        decoder.keyDecodingStrategy = .useDefaultKeys
-    }
-    #if os(Linux)
     decoder.dateDecodingStrategy = .iso8601
     decoder.keyDecodingStrategy = .useDefaultKeys
-    #endif
     return decoder
 }()
 
 func assertJSONEquivalent(_ str1: String?, _ str2: String?, file: StaticString = #file, line: UInt = #line) {
 
-    // when testing on Linux, pretty printing has slightly different
-    // meaning so the tests pass on OS X as written but need whitespace
-    // stripped to pass on Linux
-    #if os(Linux)
+    // tests pass with whitespace stripped
     var str1 = str1
     var str2 = str2
 
     str1?.removeAll { $0.isWhitespace }
     str2?.removeAll { $0.isWhitespace }
-    #endif
 
     XCTAssertEqual(
         str1,
